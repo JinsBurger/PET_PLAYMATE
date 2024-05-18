0c006be3ca53bff45f7262ebdf2b52b60f9db026
jinheon <b0b1q3v!!>
2024-05-18 15:49:36 +0900

    Delete Unnecessary Comments

--- main/main.ino
+++ main/main.ino
@@ -7,11 +7,9 @@ Move *move_impl;
 Distance *distance_impl;
 
 void setup() {
-  // Move Motor
   move_impl =  new Move(PIN_MOTOR_L1, PIN_MOTOR_L2, PIN_MOTOR_R1, PIN_MOTOR_R2);
-  // Distance Motor
   distance_impl = new Distance(PIN_ULTRA_TRIG, PIN_ULTRA_ECHO);
-  
+
   Serial.begin(9600);
 }
 
